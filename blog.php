140704afa56f7e484ba4034187b8dec2f01b624e
ValeriaG <valeria.girtie@epitech.eu>
2025-04-11 11:53:59 +0200

    fix blog and add style

---
+++ blog.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Blog</title>
+  <link rel="stylesheet" href="./src/css/main.min.css">
+  <script src="./src/dist/js/script.js" defer></script>
+  <script src="./src/dist/js/cards.js" defer></script>
+</head>
+
+<body>
+
+  <div class="card" style="top: 50px; left: 50px;">Карточка 1</div>
+  <div class="card" style="top: 200px; left: 100px;">Карточка 2</div>
+  <div class="card" style="top: 350px; left: 200px;">Карточка 3</div>
+  <canvas id="lightCanvas"> </canvas>
+  <canvas id="dotsCanvas"> </canvas>
+
+</body>
+
+</html>
