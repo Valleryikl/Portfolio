3db3ee235b76a5d0b4d32411eb3fc7a8d6f147a5
ValeriaG <valeria.girtie@epitech.eu>
2025-04-11 12:26:38 +0200

    start work of the transition

--- blog.php
+++ blog.php
@@ -7,10 +7,11 @@
   <link rel="stylesheet" href="./src/css/main.min.css">
   <script src="./src/dist/js/script.js" defer></script>
   <script src="./src/dist/js/cards.js" defer></script>
+  <script src="./src/dist/js/untransition.js" defer></script>
 </head>
 
 <body>
-
+  <div class="untransition"></div>
   <div class="card" style="top: 50px; left: 50px;">Карточка 1</div>
   <div class="card" style="top: 200px; left: 100px;">Карточка 2</div>
   <div class="card" style="top: 350px; left: 200px;">Карточка 3</div>
