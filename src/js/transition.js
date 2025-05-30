3630a936096d69338f18910f7abbeff75f52badd
ValeriaG <valeria.girtie@epitech.eu>
2025-05-30 19:37:26 +0200

    delete blog

--- src/js/transition.js
+++
@@ -1,8 +0,0 @@
-let transition = document.querySelector('.transition');
-let transitionLink = document.querySelector('#transitionLink');
-transitionLink.addEventListener('click', function() {
-    transition.style.top = "0";
-    setTimeout(() => {
-        window.location.href = "/blog.php";
-    }, 1500);
-})
